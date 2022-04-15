2a334a796a3c96161e3d9c1ccaa09050f0268d47
lodashreload <icarusjapan@gmail.com>
2022-04-15 19:39:40 -0400

    1. hard code 2.drawing UI from state

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import logo from './images/sdg-logo.png'
 
 export function App() {
-  return <div>Hello, World </div>
+  const [todoItems, setTodoItems] = useState([
+    { id: 1, text: 'Do a thing', complete: false },
+    { id: 2, text: 'Do something else', complete: false },
+    { id: 3, text: 'Do a third thing', complete: false },
+    { id: 4, text: 'Remind me about the important thing', complete: false },
+    {
+      id: 5,
+      text: 'The important things are the important things',
+      complete: false,
+    },
+  ])
+  return (
+    <div className="app">
+      <header>
+        <h1>One List</h1>
+      </header>
+      <main>
+        <ul>
+          {todoItems.map(function (todoItem) {
+            return <li key={todoItem.id}>{todoItem.text}</li>
+          })}
+        </ul>
+        <form>
+          <input type="text" placeholder="Whats up?" />
+        </form>
+      </main>
+      <footer>
+        <p>
+          <img src={logo} height="42" alt="logo" />
+        </p>
+        <p>&copy; 2020 Suncoast Developers Guild</p>
+      </footer>
+    </div>
+  )
 }
