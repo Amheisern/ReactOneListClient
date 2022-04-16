441caaba77897b57ab603acdefdb7e154d65d8c7
lodashreload <icarusjapan@gmail.com>
2022-04-16 12:45:18 -0400

    i heard you like destructing so i put a little destructing in the destructing

--- src/App.tsx
+++ src/App.tsx
@@ -104,14 +104,18 @@ type TodoItemProps = {
   todoItem: TodoItemType
   reloadItems: () => void
 }
-export function TodoItem(props: TodoItemProps) {
+export function TodoItem({
+  todoItem: { id, text, complete },
+  reloadItems,
+}: TodoItemProps) {
   //destructuring to remove repeated use of props.
-  const { todoItem, reloadItems } = props
+  // const { todoItem, reloadItems } = props
+  //further destructing defining props in the TodoItem function argument
   async function toggleCompleteStatus() {
     //This is the style often used for boolean or toggleable values
     const response = await axios.put(
-      `https://one-list-api.herokuapp.com/items/${todoItem.id}?access_token=cohort21`,
-      { item: { complete: todoItem.complete } }
+      `https://one-list-api.herokuapp.com/items/${id}?access_token=cohort21`,
+      { item: { complete: !complete } }
     )
     if (response.status === 200) {
       reloadItems()
@@ -119,10 +123,10 @@ export function TodoItem(props: TodoItemProps) {
   }
   return (
     <li
-      className={todoItem.complete ? 'completed' : ''}
+      className={complete ? 'completed' : undefined}
       onClick={toggleCompleteStatus}
     >
-      {todoItem.text}
+      {text}
     </li>
   )
 }
