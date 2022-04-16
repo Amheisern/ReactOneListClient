e9e9a7bcd752eb18a1de5819f233d424f8e50b25
lodashreload <icarusjapan@gmail.com>
2022-04-16 12:48:24 -0400

    moves todo item code into new file and new components folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import axios from 'axios'
 import React, { useState, useEffect } from 'react'
 import logo from './images/sdg-logo.png'
+import { TodoItem } from './components/TodoItemProps'
 
-type TodoItemType = {
+export type TodoItemType = {
   id: number
   text: string
   complete: boolean
@@ -97,36 +98,5 @@ export function App() {
         <p>&copy; 2020 Suncoast Developers Guild</p>
       </footer>
     </div>
-  )
-}
-
-type TodoItemProps = {
-  todoItem: TodoItemType
-  reloadItems: () => void
-}
-export function TodoItem({
-  todoItem: { id, text, complete },
-  reloadItems,
-}: TodoItemProps) {
-  //destructuring to remove repeated use of props.
-  // const { todoItem, reloadItems } = props
-  //further destructing defining props in the TodoItem function argument
-  async function toggleCompleteStatus() {
-    //This is the style often used for boolean or toggleable values
-    const response = await axios.put(
-      `https://one-list-api.herokuapp.com/items/${id}?access_token=cohort21`,
-      { item: { complete: !complete } }
-    )
-    if (response.status === 200) {
-      reloadItems()
-    }
-  }
-  return (
-    <li
-      className={complete ? 'completed' : undefined}
-      onClick={toggleCompleteStatus}
-    >
-      {text}
-    </li>
   )
 }
---
+++ src/components/TodoItemProps.tsx
@@ -0,0 +1,34 @@
+import axios from 'axios'
+import React from 'react'
+import { TodoItemType } from '../App'
+
+type TodoItemProps = {
+  todoItem: TodoItemType
+  reloadItems: () => void
+}
+export function TodoItem({
+  todoItem: { id, text, complete },
+  reloadItems,
+}: TodoItemProps) {
+  //destructuring to remove repeated use of props.
+  // const { todoItem, reloadItems } = props
+  //further destructing defining props in the TodoItem function argument
+  async function toggleCompleteStatus() {
+    //This is the style often used for boolean or toggleable values
+    const response = await axios.put(
+      `https://one-list-api.herokuapp.com/items/${id}?access_token=cohort21`,
+      { item: { complete: !complete } }
+    )
+    if (response.status === 200) {
+      reloadItems()
+    }
+  }
+  return (
+    <li
+      className={complete ? 'completed' : undefined}
+      onClick={toggleCompleteStatus}
+    >
+      {text}
+    </li>
+  )
+}
