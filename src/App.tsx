6ace0dba6422c08aee50393eec595d433b480e91
lodashreload <icarusjapan@gmail.com>
2022-04-15 21:43:29 -0400

    using useeffect with an dependency  array in order to load data for an array at start up

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,23 @@ import React, { useState, useEffect } from 'react'
 import logo from './images/sdg-logo.png'
 
 export function App() {
-  const [todoItems, setTodoItems] = useState([
-    { id: 1, text: 'Do a thing', complete: false },
-    { id: 2, text: 'Do something else', complete: false },
-    { id: 3, text: 'Do a third thing', complete: false },
-    { id: 4, text: 'Remind me about the important thing', complete: false },
-    {
-      id: 5,
-      text: 'The important things are the important things',
-      complete: false,
-    },
-  ])
+  const [todoItems, setTodoItems] = useState([])
   // We cannot use an async function directly in useEffect
   // The solution is to define the async function INSIDE and then
   // call it
   useEffect(function () {
+    //
+    //
+    // This is the async function inside the function
     async function fetchListOfItems() {
       const response = await axios.get(
         'https://one-list-api.herokuapp.com/items?access_token=cohort22'
       )
       if (response.status === 200) {
-        console.log(response.data)
+        setTodoItems(response.data)
       }
     }
+    fetchListOfItems()
   }, [])
 
   return (
