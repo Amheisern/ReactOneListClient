f47c12a09b6fa4c800106d4cd8d4ad2e9f08cfa5
lodashreload <icarusjapan@gmail.com>
2022-04-18 12:55:43 -0400

    refactored the list portion into own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import axios from 'axios'
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import logo from './images/sdg-logo.png'
-import { TodoItem } from './components/TodoItemProps'
+import { TodoList } from './TodoList'
 
 export type TodoItemType = {
   id: number
@@ -12,82 +11,13 @@ export type TodoItemType = {
 }
 
 export function App() {
-  const [todoItems, setTodoItems] = useState<TodoItemType[]>([])
-  const [newTodoText, setNewTodoText] = useState('')
-  // We cannot use an async function directly in useEffect
-  // The solution is to define the async function INSIDE and then
-  // call it
-  function loadAllTheItems() {
-    // This is the async function inside the function
-    async function fetchListOfItems() {
-      const response = await axios.get(
-        'https://one-list-api.herokuapp.com/items?access_token=cohort21'
-      )
-      if (response.status === 200) {
-        setTodoItems(response.data)
-      }
-    }
-    fetchListOfItems()
-  }
-
-  useEffect(loadAllTheItems, [])
-
-  async function handleCreateNewTodoItem() {
-    // Update handleCreateNewTodoItem to submit
-    const response = await axios.post(
-      'https://one-list-api.herokuapp.com/items?access_token=cohort21',
-      { item: { text: newTodoText } }
-    )
-    if (response.status === 201) {
-      loadAllTheItems()
-      // const refreshTodoResponse = await axios.get(
-      // 'https://one-list-api.herokuapp.com/items?access_token=cohort21'
-      // )
-      // if (refreshTodoResponse.status === 200) {
-      // setTodoItems(refreshTodoResponse.data)
-      //Clears the input field when submit is successful
-      // setNewTodoText('')
-      // const newTodo = response.data
-      // // Create a new array by *spreading* the old list and putting our new item at the end. Use [newTodo, ...todoItems] to *prepend* the new item
-      // const newTodoItems = [...todoItems, newTodo]
-      // setTodoItems(newTodoItems)
-      // }
-    }
-  }
   return (
     <div className="app">
       <header>
         <h1>One List</h1>
       </header>
       <main>
-        <ul>
-          {todoItems.map(function (todoItem) {
-            return (
-              <TodoItem
-                key={todoItem.id}
-                todoItem={todoItem}
-                reloadItems={loadAllTheItems}
-              />
-            )
-          })}
-        </ul>
-        <form
-          onSubmit={function (event) {
-            // Don't do the normal form submit (which would cause the page to refresh)
-            // since we are going to do our own thing
-            event.preventDefault()
-            handleCreateNewTodoItem()
-          }}
-        >
-          <input
-            type="text"
-            placeholder="Whats up?"
-            value={newTodoText}
-            onChange={function (event) {
-              setNewTodoText(event.target.value)
-            }}
-          />
-        </form>
+        <TodoList/>
       </main>
       <footer>
         <p>
@@ -98,3 +28,4 @@ export function App() {
     </div>
   )
 }
+
---
+++ src/TodoList.tsx
@@ -0,0 +1,78 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { TodoItem } from './components/TodoItemProps';
+import { TodoItemType } from './App';
+
+export function TodoList() {
+  const [todoItems, setTodoItems] = useState<TodoItemType[]>([]);
+  const [newTodoText, setNewTodoText] = useState('');
+  // We cannot use an async function directly in useEffect
+  // The solution is to define the async function INSIDE and then
+  // call it
+  function loadAllTheItems() {
+    // This is the async function inside the function
+    async function fetchListOfItems() {
+      const response = await axios.get(
+        'https://one-list-api.herokuapp.com/items?access_token=cohort21'
+      );
+      if (response.status === 200) {
+        setTodoItems(response.data);
+      }
+    }
+    fetchListOfItems();
+  }
+
+  useEffect(loadAllTheItems, []);
+
+  async function handleCreateNewTodoItem() {
+    // Update handleCreateNewTodoItem to submit
+    const response = await axios.post(
+      'https://one-list-api.herokuapp.com/items?access_token=cohort21',
+      { item: { text: newTodoText } }
+    );
+    if (response.status === 201) {
+      loadAllTheItems();
+      // const refreshTodoResponse = await axios.get(
+      // 'https://one-list-api.herokuapp.com/items?access_token=cohort21'
+      // )
+      // if (refreshTodoResponse.status === 200) {
+      // setTodoItems(refreshTodoResponse.data)
+      //Clears the input field when submit is successful
+      // setNewTodoText('')
+      // const newTodo = response.data
+      // // Create a new array by *spreading* the old list and putting our new item at the end. Use [newTodo, ...todoItems] to *prepend* the new item
+      // const newTodoItems = [...todoItems, newTodo]
+      // setTodoItems(newTodoItems)
+      // }
+    }
+  }
+
+  return (<>
+    <ul>
+      {todoItems.map(function (todoItem) {
+        return (
+          <TodoItem
+            key={todoItem.id}
+            todoItem={todoItem}
+            reloadItems={loadAllTheItems} />
+        );
+      })}
+    </ul>
+    <form
+      onSubmit={function (event) {
+        // Don't do the normal form submit (which would cause the page to refresh)
+        // since we are going to do our own thing
+        event.preventDefault();
+        handleCreateNewTodoItem();
+      }}
+    >
+      <input
+        type="text"
+        placeholder="Whats up?"
+        value={newTodoText}
+        onChange={function (event) {
+          setNewTodoText(event.target.value);
+        }} />
+    </form>
+  </>);
+}
